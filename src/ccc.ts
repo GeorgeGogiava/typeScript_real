c39ac2ed7fb11fff72437706076eb6c80a9429ff
GeorgeGogiava <g.gogiava@psp.local>
2024-01-04 00:15:06 +0400

    corrected string in ccc

--- src/ccc.ts
+++ src/ccc.ts
@@ -2,4 +2,5 @@ const fff:string ="GIT Is awesome!!!"
 
 const str:string ="Must be only this version"
 
-const str2:string="This is the additional version"
+const str2:string="This is the additional version, corrected to new version"
+
